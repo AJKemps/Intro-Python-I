3ea4783c98a4595bffe6c5584091d09cc0b7507b
Alex Kemper <alexkemper@MacBook-Pro.local>
2020-09-02 14:52:47 -0400

    first part of assignment done

--- src/10_functions.py
+++ src/10_functions.py
@@ -3,10 +3,19 @@
 # YOUR CODE HERE
 
 # Read a number from the keyboard
-num = input("Enter a number: ")
-num = int(num)
+num = int(input("Enter a number: "))
+# num = int()
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
 
+
+def is_even(num):
+    if num % 2 == 0:
+        print("Even!")
+    else:
+        print("Odd")
+
+
+is_even(num)
--- src/11_args.py
+++ src/11_args.py
@@ -6,23 +6,35 @@
 
 # YOUR CODE HERE
 
-print(f1(1, 2))
+def f1(a, b):
+    return a + b
 
+
+print("----------")
+print(f1(1, 2))
+print("----------")
 # Write a function f2 that takes any number of integer arguments and returns the
 # sum.
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
 
+
+def f2(*data):
+
+    return sum(data)
+
+
+print("----------")
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
 print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
-
+print("----------")
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -31,9 +43,15 @@ print(f2(a))    # Should print 22
 
 # YOUR CODE HERE
 
+
+def f3(a, b=1):
+    return a + b
+
+
+print("----------")
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
-
+print("----------")
 
 # Write a function f4 that accepts an arbitrary number of keyword arguments and
 # prints out the keys and values like so:
@@ -45,10 +63,18 @@ print(f3(8))     # Should print 9
 
 # YOUR CODE HERE
 
+
+def f4(**args):
+    for x, y in args.items():
+        print(x, y)
+
+
 # Should print
 # key: a, value: 12
 # key: b, value: 30
+print("----------")
 f4(a=12, b=30)
+
 
 # Should print
 # key: city, value: Berkeley
@@ -62,4 +88,5 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d)
+print("----------")
